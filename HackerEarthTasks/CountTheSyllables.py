555beb70c2dc359f501f4f54d487d8ab615c3b8d
MD ABRAR <mdabrar.bvb@gmail.com>
2022-03-14 18:54:47 +0530

    Create CountTheSyllables.py

---
+++ HackerEarthTasks/CountTheSyllables.py
@@ -0,0 +1,19 @@
+'''
+# Challenge
+Counting syllables
+Define a function named count that takes a single parameter. The parameter is a string. The string will contain a single word divided into syllables by hyphens, such as these:
+
+"ho-tel"
+"cat"
+"met-a-phor"
+"ter-min-a-tor"
+Your function should count the number of syllables and return it.
+
+For example, the call count("ho-tel") should return 2.
+
+Solution:
+'''
+
+def count(x):
+    syll = len(x.split("-"))
+    return syll
